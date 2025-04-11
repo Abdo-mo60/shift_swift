be0abdac92126373a635265f1cb6ecdca2827326
youssef Magdy <youssefm.ishak@gmail.com>
2025-04-11 23:55:30 +0200

    comment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:homeview/main.dart';
+import 'package:shiftswift/main.dart';
 
 
 void main() {
