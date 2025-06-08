1baf353d649fb4ebb02e9087c3d99adb337a8c4f
youssef Magdy <youssefm.ishak@gmail.com>
2025-06-08 21:48:04 +0300

    edit apply now button

--- lib/home/presentation/view/widgets/apply_now_botton.dart
+++ lib/home/presentation/view/widgets/apply_now_botton.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 import '../../../../core/service/service_locator.dart';
 import '../../../../core/styles.dart';
@@ -70,7 +68,7 @@ class ApplyNowButton extends StatelessWidget {
                     body: {
                       "jobId": jobId,
                       "memberId": Ids.memberId,
-                      "status": 1,
+                    //  "status": 1,
                     },
                   );
                 },
