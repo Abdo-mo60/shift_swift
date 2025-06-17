e896a6a4720dcd78293d4f85c316260b67e2bba3
youssef Magdy <youssefm.ishak@gmail.com>
2025-06-18 01:40:07 +0300

    edit font size

--- lib/home/presentation/view/widgets/custom_button.dart
+++ lib/home/presentation/view/widgets/custom_button.dart
@@ -35,7 +35,7 @@ class CustomButton extends StatelessWidget {
             Text(
               text,
               style: GoogleFonts.lato(
-                textStyle: AppStyles.medium20.copyWith(
+                textStyle: AppStyles.medium18.copyWith(
                   color: isIcon ? AppColors.blue : AppColors.main,
                 ),
               ),
